e0c41b380d60596eecf6e381a0e3009c6debfce6
Glenn Alex <glenn.97@gmail.com>
2018-07-04 13:29:36 +0530

    Auto Enter of Details in Appointment Activity

--- Appointments/app/src/main/java/com/gtaandteam/android/wellcure/AppointmentActivity.java
+++ Appointments/app/src/main/java/com/gtaandteam/android/wellcure/AppointmentActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -26,8 +27,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.Calendar;
@@ -44,12 +49,14 @@ public class AppointmentActivity extends AppCompatActivity {
     Toolbar toolbar;
     EditText Name,Phone,Email;
     //FirebaseUser user;
-    String user, password, first_name, email, phone,date1;
+    String user, password, first_name, email, phone,date1,name;
+    String rName,rEmail,rPhone,rDate; //retrieved files from database
     //FirebaseAuth mauth;
-    HashMap<String, String> data;
+    HashMap<String, String> data,data2;
     //FirebaseDatabase database= FirebaseDatabase.getInstance();
-    DatabaseReference databaseObject;
+    DatabaseReference databaseObject,mdb;
     int year,month,day;
+
 
 
     @Override
@@ -104,6 +111,8 @@ public class AppointmentActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+        retrieve();
+
     }
 
 
@@ -186,6 +195,54 @@ public class AppointmentActivity extends AppCompatActivity {
             }
         });
     }
+    public void retrieve() {
+        mdb = FirebaseDatabase.getInstance().getReference("users").child(fbAuth.getCurrentUser().getUid());
+        mdb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+
+                String ss = dataSnapshot.getKey().toString();
+                if (ss.equals("Name")) {
+                    rName= dataSnapshot.getValue().toString();
+                } else if (ss.equals("Email"))
+                    rEmail= dataSnapshot.getValue().toString();
+                else if (ss.equals("Phone"))
+                    rPhone= dataSnapshot.getValue().toString();
+                else if (ss.equals("Date"))
+                    rDate= dataSnapshot.getValue().toString();
+                Name.setText(rName);
+                Email.setText(rEmail);
+                Phone.setText(rPhone);
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });
+
+
+}
 
 
 
