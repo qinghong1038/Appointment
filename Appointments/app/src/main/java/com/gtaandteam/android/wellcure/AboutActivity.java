20656ff9f34ae5f9e457bdb79b182505601c9938
Pawan Bhandarkar <pawan.leo28@gmail.com>
2018-08-03 20:43:47 +0530

    Done with the about page

--- Appointments/app/src/main/java/com/gtaandteam/android/wellcure/AboutActivity.java
+++ Appointments/app/src/main/java/com/gtaandteam/android/wellcure/AboutActivity.java
@@ -1,13 +1,67 @@
 package com.gtaandteam.android.wellcure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    TextView About;
+
+    LinearLayout Youtube, Facebook, Wellcure;
+    String youtubeURL = "https://www.youtube.com/channel/UChzQns81GkjawXR9zxiHJKA";
+    String facebookURL = "https://www.facebook.com/RitusClinic/";
+    String wellcureURL = "https://www.Wellcureclinic.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        About = findViewById(R.id.about);
+        About.setText(R.string.about);
+        Youtube = findViewById(R.id.youtube);
+        Facebook = findViewById(R.id.facebook);
+        Wellcure = findViewById(R.id.wellcure);
+
+
+        Youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(youtubeURL));
+                startActivity(intent);
+
+            }
+        });
+
+        Facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(facebookURL));
+                startActivity(intent);
+
+            }
+        });
+
+        Wellcure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(wellcureURL));
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
